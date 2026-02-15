a888c43ee803d5bca82de5d693235cae53fa0974
chachaneko4649 <64012158+chachaneko4649@users.noreply.github.com>
2026-02-15 09:29:14 +0900

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,14 @@
+// 音源リスト（ここを増やすだけでOK！）
+export const soundList = [
+    { name: "たき火", file: "sounds/たき火.mp3" },
+    { name: "波の音", file: "sounds/波の音.mp3" },
+    { name: "歓声ざわめき", file: "sounds/歓声ざわめき.mp3" },
+    { name: "雨音", file: "sounds/雨音.mp3" },
+    { name: "ウグイス", file: "sounds/ウグイス.mp3" },
+    { name: "ピアノの単音", file: "sounds/ピアノの単音.mp3" },
+    { name: "ドラムソロ", file: "sounds/ドラムソロ.mp3" },
+    { name: "下駄で歩く", file: "sounds/下駄で歩く.mp3" },
+    { name: "時計の針", file: "sounds/時計の針.mp3" },
+    { name: "お金を落とす1", file: "sounds/お金を落とす1.mp3" }
+    // 好きなだけ追加してください
+];
